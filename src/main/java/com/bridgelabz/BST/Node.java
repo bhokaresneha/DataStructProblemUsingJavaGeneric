baa41ce57dc65d53ec07b081eeb5cb0871864751
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-17 22:52:27 +0530

    Added:In BST Added 56, 30 And 76 Respectivally.

---
+++ src/main/java/com/bridgelabz/BST/Node.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.BST;
+    /* Class containing left and right child of current node and key value*/
+
+class Node {
+
+    int key;
+    Node left, right;
+
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
